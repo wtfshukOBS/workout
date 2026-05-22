1df3ab3fd3b7a745b8af4f6505492a6ee5d41894
wtfshukOBS <martinsshuk@gmail.com>
2026-05-22 06:30:15 -0400

    dfg

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'workout-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ["/", "/index.html", "/ai.html"];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
